f324ed2e4bf5406ecf6fa8812f6324c98ebc50fd
sundaram4009 <62876992+sundaram4009@users.noreply.github.com>
2025-09-13 23:32:44 +0530

    Update JenkinsApplication.java

--- src/main/java/com/example/jenkins/JenkinsApplication.java
+++ src/main/java/com/example/jenkins/JenkinsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsApplication {
 
 	public static void main(String[] args) {
-		
+		//abc
 		SpringApplication.run(JenkinsApplication.class, args);
 	}
 
